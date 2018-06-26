cf995fdf7ad3d83c245aab45e33580ec25c18e53
Tung Vu <tung.vu@amperecomputing.com>
2018-06-26 09:28:02 +0700

    Ampere bmcweb: Chassis node: Add Chassis schema
    - Add chassis schema with general information:
      -- Thermal and Power and Links object.
      -- Name, ChassisType, Id property.
    - The SKU and AssetTag is not supported yet.
    
    - Tested on browser and redfishtool
      Redfishtool command test: python3 redfishtool.py -A Basic -S Always
        --rhost=$bmc_ip -u $bmc_user -p $bmc_password Chassis -I 1
    
    Change-Id: Ibfacea463ac35ab81bde1755a479611b4d6cf5a7
    Signed-off-by: Tung Vu <tung.vu@amperecomputing.com>

--- redfish-core/lib/ocp-chassis.hpp
+++ redfish-core/lib/ocp-chassis.hpp
@@ -29,12 +29,112 @@
 namespace redfish {
 
 /**
+ * D-Bus types primitives for several generic D-Bus interfaces
+ * TODO consider move this to separate file into boost::dbus
+ */
+using ManagedObjectsType = std::vector<
+    std::pair<dbus::object_path,
+              std::vector<std::pair<
+                  std::string,
+                  std::vector<std::pair<std::string, dbus::dbus_variant>>>>>>;
+
+using PropertiesType =
+    boost::container::flat_map<std::string, dbus::dbus_variant>;
+
+/**
+* ChassisAsyncResp
+* Gathers data needed for response processing after async calls are done
+*/
+class ChassisAsyncResp {
+public:
+ ChassisAsyncResp(crow::response& response): res(response) {}
+
+ ~ChassisAsyncResp() {
+   if (res.code != static_cast<int>(HttpRespCode::OK)) {
+     // Reset the json object to clear out any data that made it in before the
+     // error happened
+     // TODO handle error condition with proper code
+     res.json_value = nlohmann::json::object();
+   }
+   res.end();
+ }
+ void setErrorStatus() {
+   res.code = static_cast<int>(HttpRespCode::INTERNAL_ERROR);
+ }
+
+ crow::response& res;
+};
+
+/**
+ * Chassis override class for delivering Chassis Schema
+ */
+class Chassis : public Node {
+ public:
+  /*
+   * Default Constructor
+   */
+  template <typename CrowApp>
+  Chassis(CrowApp &app)
+      : Node(app, "/redfish/v1/Chassis/1/") {
+    Node::json["@odata.type"] = "#Chassis.v1_4_0.Chassis";
+    Node::json["@odata.id"] = "/redfish/v1/Chassis/1";
+    Node::json["@odata.context"] = "/redfish/v1/$metadata#Chassis.Chassis";
+    Node::json["Name"] = "Ampere System Chassis"; // TODO hardcode in temporary.
+    Node::json["ChassisType"] = "Rack Mount Chassis";
+    Node::json["Id"] = "1";
+    // TODO Currently not support "SKU" and "AssetTag" yet
+    Node::json["SKU"] = "";
+    Node::json["AssetTag"] = "";
+
+    entityPrivileges = {{crow::HTTPMethod::GET, {{"Login"}}},
+                        {crow::HTTPMethod::HEAD, {{"Login"}}},
+                        {crow::HTTPMethod::PATCH, {{"ConfigureComponents"}}},
+                        {crow::HTTPMethod::PUT, {{"ConfigureComponents"}}},
+                        {crow::HTTPMethod::DELETE, {{"ConfigureComponents"}}},
+                        {crow::HTTPMethod::POST, {{"ConfigureComponents"}}}};
+  }
+
+ private:
+  /**
+   * Functions triggers appropriate requests on D-Bus
+   */
+  void doGet(crow::response &res, const crow::request &req,
+             const std::vector<std::string> &params) override {
+    auto asyncResp = std::make_shared<ChassisAsyncResp>(res);
+
+    // Create JSON copy based on Node::json, this is to avoid possible
+    // race condition
+    nlohmann::json json_response(Node::json);
+
+    // Thermal object
+    json_response["Thermal"] = {
+                        {"@odata.id", "/redfish/v1/Chassis/1/Thermal"}};
+    // Power object
+    json_response["Power"] = {
+                        {"@odata.id", "/redfish/v1/Chassis/1/Power"}};
+    // An array of references to computer systems contained in this chassis.
+    json_response["Links"]["ComputerSystems"] =
+                              {{{"@odata.id", "/redfish/v1/Systems/1"}}};
+    // An array of references to the Managers responsible
+    // for managing this chassis.
+    json_response["Links"]["ManagedBy"] =
+                            {{{"@odata.id", "/redfish/v1/Managers/1"}}};
+
+    asyncResp->res.json_value = json_response;
+
+  }
+
+  // TODO Chassis Provider object will be declared here
+};
+
+/**
  * ChassisCollection derived class for delivering Chassis Collection Schema
  */
 class ChassisCollection : public Node {
  public:
   template <typename CrowApp>
-  ChassisCollection(CrowApp &app): Node(app, "/redfish/v1/Chassis/") {
+  ChassisCollection(CrowApp &app)
+    : Node(app, "/redfish/v1/Chassis/"), memberChassis(app) {
     Node::json["@odata.type"] = "#ChassisCollection.ChassisCollection";
     Node::json["@odata.id"] = "/redfish/v1/Chassis";
     Node::json["@odata.context"] =
@@ -64,6 +164,7 @@ class ChassisCollection : public Node {
     res.end();
   }
 
-  // TODO The chassis member will be declared here.
+  // The Chassis object as member of ChassisCollection.
+  Chassis memberChassis;
 };
 }  // namespace redfish
