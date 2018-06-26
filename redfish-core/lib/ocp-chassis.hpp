2675f9b68158028bcd5b3534e412d11fc5d9992f
Tung Vu <tung.vu@amperecomputing.com>
2018-06-26 16:37:58 +0700

    Ampere bmcweb: Chassis node: Revise License field
    Add copyright of Intel
    Remove Author information
    Separate file explanation from license field
    
    Tested: This change does not impact to supported items.
    Command test: redfishtool -A Basic -S Always --rhost=$bmc_ip -u
        $bmc_user -p $bmc_password Chassis -I 1
    
    Change-Id: I688c3c8e2c122366eb99a4df1c854e35e0089e88
    Signed-off-by: Tung Vu <tung.vu@amperecomputing.com>

--- redfish-core/lib/ocp-chassis.hpp
+++ redfish-core/lib/ocp-chassis.hpp
@@ -1,6 +1,6 @@
 /*
+// Copyright (c) 2018 Intel Corporation
 // Copyright (c) 2018 Ampere Computing LLC
-// Author: Tung Vu <tung.vu@amperecomputing.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+*/
+
+/*
 // The OCP chassis module supports to retrieve chassis inventory via D-Bus.
 // It's used to replace the redfish-core/lib/chassis.hpp module
 // (https://github.com/openbmc/bmcweb).
